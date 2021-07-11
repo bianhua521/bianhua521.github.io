ac90706fb9d30fca95ced557c7bcd482f58a085f
HayFuon <2964517993@qq.com>
2021-07-11 20:49:44 +0800

    ...

--- static/js/main.js
+++ static/js/main.js
@@ -30913,7 +30913,7 @@ try{
               if (this.decoderPending) return this.decoderPending;
               var e = "object" != typeof WebAssembly || "js" === this.decoderConfig.type,
                   t = [];
-              return e ? t.push(this._loadLibrary("https://haycar.hayfuon.icu/static/js/decoder.js", "text")) : (t.push(this._loadLibrary("js/wrapper.js", "text")), t.push(this._loadLibrary("decoder.wasm", "arraybuffer"))), this.decoderPending = Promise.all(t).then(t => {
+              return e ? t.push(this._loadLibrary("/static/js/decoder.js", "text")) : (t.push(this._loadLibrary("/static/js/wrapper.js", "text")), t.push(this._loadLibrary("decoder.wasm", "arraybuffer"))), this.decoderPending = Promise.all(t).then(t => {
                   var i = t[0];
                   e || (this.decoderConfig.wasmBinary = t[1]);
                   var n = Lf.DRACOWorker.toString(),
